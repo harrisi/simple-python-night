17f924b9e30ada0e4833ed4a92efadf6b64ffa9e
Ian Harris <ian+gh@dgby.org>
2016-09-08 19:22:53 -0700

    Moved instructions around
    I don't know if there's any use of having two (and potential more later)
    dictionaries for different types of instructions or not.
    
    Probably not.

--- src/machine.py
+++ src/machine.py
@@ -10,6 +10,9 @@ SUB
 PUSH -1
 MUL'''
 
+# ugly silly global stack because why bother
+stack = []
+
 # I think this will be over engineered for two hours of time to spend.
 class vm(object):
     def __init__(self, prog): # optional delimiter?
@@ -20,25 +23,37 @@ ops = {'ADD': operator.add,
        'MUL': operator.mul,
        'DIV': operator.truediv}
 
+def PUSH(operands):
+    # this requires reading an operand. Doing this in general means either
+    # reading the actual input as a stream or passing all operands to all
+    # functions. I think that's a reasonable solution, actually.
+    global stack
+    stack.append(int(operands[0]))
+
+def POP(operands):
+    global stack
+    return stack.pop()
+
+instructions = {'PUSH': PUSH,
+                'POP':  POP}
 
 def read_instr(prog):
     prog_list = prog.split('\n') # split program into instructions (seperated by
                                  # line)
-    stack = []
+    global stack
     op_left = 0
     for inst in prog_list:
         cur_inst = inst.split()
-        if inst in ops:
+        if cur_inst[0] in ops:
             stack.append(ops[cur_inst[0]](stack.pop(), stack.pop()))
-        elif cur_inst[0] == 'PUSH':
-            stack.append(int(cur_inst[1]))
-        elif cur_inst[0] == 'POP':
-            stack.pop()
-    return stack
+        elif cur_inst[0] in instructions:
+            instructions[cur_inst[0]](cur_inst[1:])
+        else:
+            print('unknown instruction!')
 
 if __name__ == '__main__':
     # read instructions, but for now I would rather just execute a static
     # program, for quick progression.
     print(program)
-    print(read_instr(program))
-
+    read_instr(program)
+    print(stack)
